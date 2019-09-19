d5d55e770c564f2fc04f9a5792faa33feb9ff0d6
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-09-19 18:01:32 -0300

    Lesson 02.06 Refactoring code

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Transferencias")),
+        appBar: AppBar(title: Text("Transferências")),
+        body: ListTransfers(),
         floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+class ListTransfers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferCard(Transfer(100.0, 10000)),
+        TransferCard(Transfer(200.0, 10000)),
+        TransferCard(Transfer(300.0, 10000)),
+      ],
+    );
+  }
+}
+
+class TransferCard extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferCard(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.billNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int billNumber;
+
+  Transfer(this.value, this.billNumber);
+}
+
